dd8e9bd95ae4f53310dbe6472e145c572d29838e
Rahulwagh228 <127666835+Rahulwagh228@users.noreply.github.com>
2024-05-21 13:46:51 +0530

    api endpoint modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apikey=58f95926";
+const API_URL = "https://www.omdbapi.com?apikey=58f95926";
 
 
 function App() {
